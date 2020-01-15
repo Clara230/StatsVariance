31369a51a3b2051d1c44207fbfe0a07df4ed2323
clara9621 <58587888+clara9621@users.noreply.github.com>
2020-01-15 11:25:43 +0200

    Add project files.

---
+++ Calculator/FindVariance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using StatsOperations;
+
+
+
+namespace StatisticsVariance
+
+{
+
+    public class FindVariance
+
+    {
+
+        public static dynamic result;
+
+        public double Variance(dynamic arrayA)
+
+        {
+
+            result = StatsOperations.StatsVariance.Variance(arrayA);
+
+            return result;
+
+        }
+
+    }
+
+}
